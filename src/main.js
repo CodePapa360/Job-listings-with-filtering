5919b2ebe8ee16dfe006dbba2b16ef8f56b53a57
Alamin <codepapa360@gmail.com>
2023-08-10 23:14:13 +0600

    starting-state-functionality

--- src/main.js
+++ src/main.js
@@ -14,21 +14,42 @@ function filtering(filters) {
   );
 }
 
-let currentStates = [];
+let currentStates = {
+  role: null,
+  level: null,
+  tools: [],
+  languages: [],
+};
 
 jobsContainer.addEventListener("click", (e) => {
   const btn = e.target.closest(".skill");
   if (!btn) return;
   const { catagory } = btn.dataset;
   const { field } = btn.dataset;
+  const has = 
 
-  currentStates.push({ [catagory]: field });
+  // currentStates.push({ [catagory]: field });
   // console.log(currentStates);
-  const newList = filtering(currentStates);
+  // const newList = filtering(currentStates);
   // console.log(newList);
 
-  renderJobs(newList);
-  renderFilterBar(currentStates);
+  // renderJobs(newList);
+  // renderFilterBar(currentStates);
 });
 
 renderJobs(dataJson);
+
+// let test = [{ a: 1 }, { b: 2 }, { c: 3 }];
+
+// test.forEach((obj) => {
+//   const rem = 2;
+
+//   const target = Object.values(obj)[0] === rem;
+//   console.log(target);
+// });
+
+// const var1 = ["a", "b", "c"];
+// const var2 = ["p", "d"];
+
+// const test = var1.some((varss) => var2.includes(varss));
+// console.log(test);
